0c636119ffc43a86b7a5814c2f9b7b336617eb7d
P <30462819+ptrpfa@users.noreply.github.com>
2024-03-30 08:10:19 +0000

    cleaned aggregation of datasets and standardised split parameters

--- config.py
+++ config.py
@@ -1,4 +1,9 @@
 """ Configurations """
+# Folders
 EXPORT_FOLDER = "./export"
 # EXPORT_FOLDER = "./dataset/Processed_Dataset" # Old path
 MODEL_FOLDER = "./models"
+
+# Parameters
+TEST_SIZE = 0.3
+RANDOM_STATE = 123456
